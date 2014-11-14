ec87ad1d171d2d37f14c162343c88307330b5026
mliszcz <liszcz.michal@gmail.com>
2014-11-14 18:00:18 +0100

    working libkvm

---
+++ controller/src/controller.cpp
@@ -0,0 +1,38 @@
+
+// #include <linux/kvm.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <cstdio>
+#include <cstring>
+
+#include "libkvm.hpp"
+
+// https://www.kernel.org/doc/Documentation/virtual/kvm/api.txt
+
+int main(int argc, char** argv) {
+
+	kvm_callbacks callbacks;
+
+	kvm_context_t kvm_ctx = kvm_init(&callbacks, nullptr);
+
+	printf("ctx: %d\n", kvm_ctx);
+
+	// int fd = open("/dev/kvm", O_RDWR);
+	// if (fd == -1) {
+	// 	perror("open /dev/kvm");
+	// 	return 1;
+	// }
+	
+	// int r = ioctl(fd, KVM_GET_API_VERSION, 0);
+	// if (r == -1) {
+	// 	fprintf(stderr, "kvm kernel version too old: KVM_GET_API_VERSION ioctl not supported\n");
+	// 	return 1;
+	// }
+
+	// printf("api ver: %d\n", r);
+
+	kvm_finalize(kvm_ctx);
+
+	return 0;
+}
